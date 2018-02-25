bbd718068cf3a594b53d2ffb11b81671bed3cde1
Pascha <atom.tomcat@gmail.com>
2018-02-25 16:39:53 +0300

    custom collection view added but without functionaluty

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Pascha on 25.02.2018.
+//  Copyright © 2018 Pascha. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
